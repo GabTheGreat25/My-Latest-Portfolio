4c748419dd1475d0be27e43c7ffbd32d1316ebbb
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-10-10 21:22:12 +0800

    Add animation in footer

--- src/sections/footer.tsx
+++ src/sections/footer.tsx
@@ -6,20 +6,20 @@ import { ArrowUpRight, DoubleArrowUp } from "@/assets/icons";
 
 const footerLinks = [
   {
-    title: "Youtube",
-    href: "#",
+    title: "Github",
+    href: "https://github.com/GabTheGreat25",
+  },
+  {
+    title: "Linkedin",
+    href: "https://www.linkedin.com/in/gaabsyy25",
+  },
+  {
+    title: "Facebook",
+    href: "https://www.facebook.com/Codeeee_is_Lifeeee",
   },
   {
     title: "Twitter",
-    href: "#",
-  },
-  {
-    title: "Instagram",
-    href: "#",
-  },
-  {
-    title: "LinkedIn",
-    href: "#",
+    href: "https://twitter.com/Gaabsyy25",
   },
 ];
 
@@ -28,6 +28,8 @@ export function Footer({
 }: {
   handleLinkClick: (link: string) => void;
 }) {
+  const [hoveredButton, setHoveredButton] = React.useState<string | null>(null);
+
   return (
     <footer className="relative z-10 overflow-x-clip">
       <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-success-secondary/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10"></div>
@@ -40,19 +42,29 @@ export function Footer({
           </a>
         </div>
         <div className="border-t border-light-default/15 sm:pb-6 pb-0 text-sm flex flex-col md:flex-row md:justify-between items-center gap-x-8">
-          <div className="inline-flex items-center text-light-default/40">
+          <div className="inline-flex items-center text-light-default">
             <Image src="/logo.png" alt="logo" width="100" height="100" />
             <span>&copy; 2024. All rights reserved.</span>
           </div>
-          <nav className="flex flex-col md:flex-row items-center gap-8 md:gap-5 lg:gap-4">
+          <nav className="flex flex-col md:flex-row items-center gap-8 md:gap-5 lg:gap-4 mb-6 md:mb-0">
             {footerLinks.map((link) => (
               <a
                 key={link.title}
                 href={link.href}
-                className="inline-flex items-center gap-1.5"
+                target="_blank"
+                rel="noreferrer"
+                className="inline-flex items-center gap-1.5 hover:scale-110 duration-500 ease-in-out"
+                onMouseEnter={() => setHoveredButton(link.title)}
+                onMouseLeave={() => setHoveredButton(null)}
               >
                 <span className="font-semibold">{link.title}</span>
-                <ArrowUpRight className="size-4" />
+                <ArrowUpRight
+                  className={`size-4 transition-transform duration-700 ${
+                    hoveredButton === link.title
+                      ? "rotate-[225deg]"
+                      : "rotate-0"
+                  }`}
+                />
               </a>
             ))}
           </nav>
